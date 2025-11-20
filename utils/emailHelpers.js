0aeb684f6184ef30e3a94f0302ecfa1669c0af5e
Dynamate-Tech <dynamate.promo@gmail.com>
2025-11-20 16:29:32 +0800

    Update emailHelpers.js

--- utils/emailHelpers.js
+++ utils/emailHelpers.js
@@ -11,15 +11,20 @@ export async function sendEmail({ to, subject, html }) {
     }
   });
 
-  await transporter.sendMail({
-    from: '"Promo Price App" <your.email@gmail.com>',
+  try {
+    await transporter.sendMail({
+    from: \`"Promo Price App" <${process.env.EMAIL_USER}>\`,
     to,
     subject,
     html,
   });
-
-  console.log(`✅ Email sent to ${to} with subject "${subject}"`);
-}
+  console.log(\`✅ Email sent to ${to} with subject "${subject}"\`);
+  } catch (error) {
+    console.error("Nodemailer Error:", error.message);
+    // Log the full error object for detailed debugging
+    // console.error("Nodemailer Full Error:", error); 
+    throw new Error(\`Failed to send email: \${error.message}\`);
+  }}
 
 
 
